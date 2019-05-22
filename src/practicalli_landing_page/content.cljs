eb84d73005dd6208050ca01b3ed567cb40eb058d
John Stevenson <john@jr0cket.co.uk>
2019-05-22 19:30:44 +0100

    Helper function to separate sections and tag with ids for navbar

--- src/practicalli_landing_page/content.cljs
+++ src/practicalli_landing_page/content.cljs
@@ -170,3 +170,17 @@
           [:img {:src "images/youtube-subscribe-button.jpg"}]]]]
        ]]]]])
 
+
+;; helper functions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defn level-separator
+  "A separator to provide a gap between components.
+  The separator takes an id so that the section
+  does not get hidden by the menu when linking"
+  [identifier]
+  [:div {:class "level"
+         :id    identifier}
+   [:h5 {:class "is-size-5 is-invisible"}
+    identifier]])
