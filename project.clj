3e2c8efa8e557ed465ed73380238c980b0a23083
John Stevenson <john@jr0cket.co.uk>
2019-05-26 01:00:35 +0100

    Added live profile to create single js file in /docs directory
    Added a live alias to the figwheel-main build configuration.
    
    Added an alias to call a minified build using the live configuration.
    
    Added a live.cljs.edn configuration to output the javascript file to the
    /docs/cljs-out/ directory.

--- project.clj
+++ project.clj
@@ -15,8 +15,12 @@
   :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
             "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
             "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]
+            "fig:live"  ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "live"]
             "fig:test"  ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" practicalli-landing-page.test-runner]}
 
-  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.1.9"]
-                                  [com.bhauman/rebel-readline-cljs "0.1.4"]]
-                   }})
+  :profiles {:dev  {:dependencies [[com.bhauman/figwheel-main "0.1.9"]
+                                   [com.bhauman/rebel-readline-cljs "0.1.4"]]
+                    }
+             :live {:dependencies [[com.bhauman/figwheel-main "0.1.9"]
+                                   [com.bhauman/rebel-readline-cljs "0.1.4"]]
+                    }})
