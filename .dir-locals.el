72793f162a7b2eb6a1282c2e132314b6de7fcdd0
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2021-09-12 18:40:18 +0100

    project: Emacs dir-locals.el include figwheel alias, run dev build
    cider-jack-in-cljs includes user prompts for the type of project, build tool and
    build profile to use when starting a ClojureScript REPL
    
    Add default values for the prompts so they do not appear
    
    `cider-preferred-build-tool`
    - use Clojure CLI tools to start the REPL process
    
    `cider-clojure-cli-aliases`
    - alias names to include in the command to start the REPL process, adding the
    figwheel-main dependency
    
    `cider-default-cljs-repl`
    - the ClojureScript build tool to use
    
    `cider-figwheel-main-default-options`
    - set the build to run, matching build-name.cljs.edn build profile file
    
    `cider-repl-display-help-banner`
    - do not print the help banner in the REPL buffer

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((clojure-mode . ((cider-preferred-build-tool          . clojure-cli)
+                  (cider-clojure-cli-aliases           . ":env/figwheel")
+                  (cider-default-cljs-repl             . figwheel-main)
+                  (cider-figwheel-main-default-options . "dev")
+                  (cider-repl-display-help-banner      . nil))))
